8ec61babe421bb64d8a5a034aeeefbc84a4f7c50
Luisa <luisalacerdamendes@gmail.com>
2024-01-18 08:02:09 -0300

    Criação da header

---
+++ frontend/src/app/components/partials/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent implements OnInit{
+
+  constructor() {}
+
+  ngOnInit(): void {
+    
+  }
+
+}
