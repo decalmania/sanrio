18516da9d9ea03b1689ba2317e831923f5ca5434
Luísa <132020235+decalmania@users.noreply.github.com>
2024-02-05 13:34:03 -0300

    Correção da configuração

--- frontend/src/app/app.config.server.ts
+++ frontend/src/app/app.config.server.ts
@@ -1,7 +1,6 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
-import { ToastrService } from 'ngx-toastr';
 
 const serverConfig: ApplicationConfig = {
   providers: [
